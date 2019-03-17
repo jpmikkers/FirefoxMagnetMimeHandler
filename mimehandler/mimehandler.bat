bed6059a74f8bd047d2d348a91fcfa8e0d1df4a0
Jean-Paul Mikkers <jean.paul.mikkers@gmail.com>
2019-03-17 01:04:42 +0100

    Fix executionpolicy

--- mimehandler/mimehandler.bat
+++ mimehandler/mimehandler.bat
@@ -3,5 +3,5 @@ REM echo running script "%~f0"
 REM echo with argument "%*"
 REM pause
 pushd %~dp0
-PowerShell -ExecutionPolicy RemoteSigned -File mimehandler.ps1 "%*"
+PowerShell -ExecutionPolicy Unrestricted -File mimehandler.ps1 "%*"
 popd
